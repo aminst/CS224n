fe79fc8bf13001d463da9175af3fb7ba3deb2153
aminst <aminst79@gmail.com>
2021-08-26 17:12:34 +0430

    Complete A3

--- A3/a3/parser_model.py
+++ A3/a3/parser_model.py
@@ -72,7 +72,11 @@ class ParserModel(nn.Module):
         ###     Dropout: https://pytorch.org/docs/stable/nn.html#dropout-layers
         ### 
         ### See the PDF for hints.
-
+        self.embed_to_hidden_weight = nn.Parameter(nn.init.xavier_uniform_(torch.empty(self.embed_size * self.n_features, self.hidden_size)))
+        self.embed_to_hidden_bias = nn.Parameter(nn.init.uniform_(torch.empty(self.hidden_size)))
+        self.dropout = nn.Dropout(self.dropout_prob)
+        self.hidden_to_logits_weight = nn.Parameter(nn.init.xavier_uniform_(torch.empty(self.hidden_size, self.n_classes)))
+        self.hidden_to_logits_bias = nn.Parameter(nn.init.uniform_(torch.empty(self.n_classes)))
 
 
 
@@ -106,8 +110,10 @@ class ParserModel(nn.Module):
         ###     Gather: https://pytorch.org/docs/stable/torch.html#torch.gather
         ###     View: https://pytorch.org/docs/stable/tensors.html#torch.Tensor.view
         ###     Flatten: https://pytorch.org/docs/stable/generated/torch.flatten.html
-
-
+        
+        x = torch.Tensor(w.shape[0], self.embed_size * self.n_features)
+        for idx, i in enumerate(w):
+            x[idx] = self.embeddings[i].view(self.embed_size * self.n_features)
 
         ### END YOUR CODE
         return x
@@ -143,7 +149,10 @@ class ParserModel(nn.Module):
         ### Please see the following docs for support:
         ###     Matrix product: https://pytorch.org/docs/stable/torch.html#torch.matmul
         ###     ReLU: https://pytorch.org/docs/stable/nn.html?highlight=relu#torch.nn.functional.relu
-
+        x = self.embedding_lookup(w)
+        before_relu = torch.matmul(x,self.embed_to_hidden_weight) + self.embed_to_hidden_bias
+        h = torch.relu(before_relu)
+        logits = torch.matmul(h, self.hidden_to_logits_weight) + self.hidden_to_logits_bias
 
         ### END YOUR CODE
         return logits
